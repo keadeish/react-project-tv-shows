022f8ae0269698c5af72a1b2ca8658b920d05aed
Keadeish <keadeish10@hotmail.com>
2022-08-04 21:35:35 +0100

    created a drop-down list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import episodes from "./episodes.json";
+import episodes from "./episodes.json";
 import EpisodesDisplay from "./Components/EpisodesDisplay";
 import { useEffect, useState } from "react";
 import { isMatching } from "./utils/matchingEpisodes";
@@ -8,6 +8,8 @@ function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
   console.log("inside the App function");
   const [episodesData, setEpisodesData] = useState<IEpisode[]>([]);
+  // function fetchEpisodesFromURL(episodeID : number) {
+    
   useEffect(() => {
     const getAndStoreDataFromApi = async () => {
       const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
@@ -17,11 +19,16 @@ function App(): JSX.Element {
     };
     getAndStoreDataFromApi();
   }, []);
-  const filteredEpisodes = isMatching(episodesData, searchTerm);
-
+// }
+const filteredEpisodes = isMatching(episodesData, searchTerm);
   return (
     <>
       <div>
+      <select /*onClick={()=>(fetchEpisodesFromURL)}*/>
+     {episodes.map((show) => (
+    <option key ={show.id} value={show.id}>{show.name}</option>
+    ))}
+    </select>
         <input
           value={searchTerm}
           onChange={(e) => {
